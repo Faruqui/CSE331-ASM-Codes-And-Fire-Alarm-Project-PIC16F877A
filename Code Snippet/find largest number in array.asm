9403130c2b6888e589a213ddbb093c7e4c544229
Faruqui <30295498+Faruqui@users.noreply.github.com>
2019-11-07 21:32:21 +0600

    Add files via upload

--- Code Snippet/find largest number in array.asm
+++ Code Snippet/find largest number in array.asm
@@ -5,48 +5,51 @@ INCLUDE "EMU8086.INC"
 
 .DATA
     
-    ARR DB 5, 4, 3, 7, 9
-    MAX DB ?
-    COUNT DB 4
+    ARR DB 5, 4, 3, 7, 9    ;Initilize array
+    MAX DB ?                ;create MAX variable
+    COUNT DB 4              ;initialize counter
 
 .CODE
 MAIN PROC
-    MOV AX, @DATA
+    MOV AX, @DATA           ;load data
     MOV DS, AX
     
-    LEA BX, ARR
+    LEA BX, ARR             ;load the address of the array onto BX register
+    
      
-    PRINT "THE 1ST NUMBER IS: " 
+    PRINT "THE 1ST NUMBER IS: " ;print 1st number of the array
     MOV AH, 2
     MOV DL, [BX]
     ADD DL, 48
     INT 21H
     
-    MOV DL, [BX]
-    MOV MAX, DL
-    MOV SI, 1H
+    MOV DL, [BX]            ;load the 1st number of array
+    MOV MAX, DL             ;store the number inside variable MAX
+    MOV SI, 1H              ;set indexing register valu to 1
     
     @LOOP:
-        MOV DL, MAX
-        MOV AL, [BX+SI]
-        CMP DL, AL
-        JGE GREATER
-        JL LESS
+        MOV DL, MAX         ;move MAX to DL register
+        MOV AL, [BX+SI]     ;move current index number onto AL
+        CMP DL, AL          ;compare MAX and current number
+        JGE GREATER         ;if MAX is greater or equal
+        JL LESS             ;if MAX is smaller that current number
         
-        LESS:
+        LESS:               ;set current number as MAX
             MOV DL, [BX+SI]
             MOV MAX, DL
             JMP LOOP_END
+            
         GREATER:
             JMP LOOP_END
             
-        LOOP_END:
-            INC SI
-            DEC COUNT
-            CMP COUNT, 0
-            JNZ @LOOP
+    LOOP_END:
+        INC SI              ;increase Index number
+        DEC COUNT           ;decrease counter   c--
+        CMP COUNT, 0        ;c>=0
+        JNZ @LOOP
             
-       PRINTN
+       
+       PRINTN               ;print greatest number
        PRINT "THE GREATEST NUMBER IS: "
        MOV AH, 2
        MOV DL, MAX
