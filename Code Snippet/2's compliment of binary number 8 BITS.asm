fc1d91cebe1d2eb558dbb643c4edfcee78b1774b
Faruqui <30295498+Faruqui@users.noreply.github.com>
2019-11-08 08:15:39 +0600

    Add files via upload

--- Code Snippet/2's compliment of binary number 8 BITS.asm
+++ Code Snippet/2's compliment of binary number 8 BITS.asm
@@ -2,8 +2,7 @@
 .STACK 100H
 
 .DATA
-    NUM DB 00001011B    ;Initilize a 8bit binary number
-    COUNT DB 8         ;count = 8
+    NUM DB 00011000B    ;Initilize a 8bit binary number
     
 .CODE
 MAIN PROC
@@ -14,6 +13,7 @@ MAIN PROC
     MOV BH, 00H
     NOT BL               ; invert the bits
     INC BL               ; add 1
+    MOV CX, 8
     
     MOV AH, 2
     LOOP:
@@ -30,7 +30,7 @@ MAIN PROC
         INT 21H
         
         END_LOOP:
-        DEC COUNT         ; count--
+        DEC CX            ; count--
         JNZ LOOP          ; jump until count is 0
         
     
